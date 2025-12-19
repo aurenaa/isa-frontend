fc874b1862d94271884f86897d5fca4fa4e3e83e
aurenaa <salapuranev@gmail.com>
2025-12-19 12:30:40 +0100

    feat: Added newest videos row and scroll and browse videos

--- isa/src/app/app.component.ts
+++ isa/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './service/auth.service';
 
@@ -23,5 +23,34 @@ export class AppComponent implements OnInit {
 
   logout(): void {
     this.authService.logout().subscribe();
+  }
+
+    @ViewChild('newestRow') newestRow!: ElementRef;
+
+  newestVideos = [
+    { title: 'Newest video 1' },
+    { title: 'Newest video 2' },
+    { title: 'Newest video 3' },
+    { title: 'Newest video 4' },
+    { title: 'Newest video 5' }
+  ];
+
+  browseVideos = [
+    { title: 'Browse video 1' },
+    { title: 'Browse video 2' },
+    { title: 'Browse video 3' },
+    { title: 'Browse video 4' },
+    { title: 'Browse video 5' },
+    { title: 'Browse video 6' },
+    { title: 'Browse video 7' },
+    { title: 'Browse video 8' }
+  ];
+
+  scrollLeft() {
+    this.newestRow.nativeElement.scrollLeft -= 380;
+  }
+
+  scrollRight() {
+    this.newestRow.nativeElement.scrollLeft += 380;
   }
 }
