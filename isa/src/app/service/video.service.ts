1339b45b8d70511019b1acc3530b17c25dfc6f19
Pavicevic Tea <pavicevictea6@gmail.com>
2026-01-07 03:21:15 +0100

    feat: service class for videos added

---
+++ isa/src/app/service/video.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VideoService {
+    private _api_url = 'http://localhost:8080/api';
+    private _video_url = this._api_url + '/videos';
+
+    constructor(private http: HttpClient) {}
+
+    uploadVideo(videoData: any, videoFile: File, thumbnailFile: File): Observable<any> {
+        const formData = new FormData();
+
+        formData.append('data', new Blob([JSON.stringify(videoData)], {
+            type: 'application/json'
+        }));
+
+        formData.append('video', videoFile);
+        formData.append('thumbnail', thumbnailFile);
+
+        return this.http.post('${this._video_url}./upload', formData);
+    }
+}
