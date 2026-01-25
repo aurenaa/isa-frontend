8f6d6835b64ff99b95c855de06e7ce3a12e3025d
Pavicevic Tea <pavicevictea6@gmail.com>
2026-01-25 02:43:51 +0100

    feat: commenting feature implemented on frontend

--- isa/src/app/video/video.component.ts
+++ isa/src/app/video/video.component.ts
@@ -1,3 +1,4 @@
+import { RouterTestingModule } from '@angular/router/testing';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { VideoService } from '../service/video.service';
@@ -14,11 +15,19 @@ export class VideoComponent implements OnInit, OnDestroy {
   video: any;
   private socketSubscription: Subscription | undefined;
 
+  comments: any[] = [];
+  newCommentText: string = '';
+
+  currentPage: number = 0;
+  pageSize: number = 10;
+  totalComments: number = 0;
+  isLastPage: boolean = false;
+
   constructor( 
     private route: ActivatedRoute, 
     private videoService: VideoService, 
     private socketService: SocketService,
-    private CommentService: CommentService
+    private commentService: CommentService
   ) {}
 
   ngOnInit(): void {
@@ -28,6 +37,7 @@ export class VideoComponent implements OnInit, OnDestroy {
 
       this.videoService.getVideoDetails(videoId).subscribe(data => {
         this.video = data;
+        this.loadComments(videoId);
       });
 
       this.videoService.incrementView(videoId).subscribe({
@@ -88,6 +98,40 @@ export class VideoComponent implements OnInit, OnDestroy {
         if (err.status === 401) alert("You have to be logged in to dislike the video!");
       }
     });
+  }
+
+  loadComments(videoId: number, append: boolean = false): void {
+    this.commentService.getVideoComments(videoId, this.currentPage, this.pageSize).subscribe(data => {
+      if (append) {
+        this.comments = [...this.comments, ...data.content];
+      } else {
+        this.comments = data.content;
+      }
+      this.totalComments = data.totalElements;
+      this.isLastPage = data.last;
+    });
+  }
+
+  submitComment(): void {
+    if (!this.newCommentText.trim()) return;
+    this.commentService.addComment(this.video.id, this.newCommentText).subscribe({
+      next: () => {
+        this.newCommentText = '';
+        this.currentPage = 0;
+        this.loadComments(this.video.id, false);
+      }, 
+      error: (err) => {
+        console.error(err);
+        alert(err.error || "Error submitting comment");
+      }
+    });
+  }
+
+  loadNextPage(): void {
+    if(!this.isLastPage) {
+      this.currentPage++;
+      this.loadComments(this.video.id, true);
+    }
   }
   
 }
